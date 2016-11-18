907f361221154a876369bd6c63bcde3392188202
Richard Protasov <richardprotasov@gmail.com>
2016-11-18 11:43:10 -0500

    Include nerdtree and vim-sensible

---
+++ vim-remote.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+git clone git://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible
+
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+echo "execute pathogen#infect()" > ~/.vimrc
