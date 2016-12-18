2c4b56292f78cdde776ff2bb5b129bc94e867a1d
Richard Protasov <richardprotasov@gmail.com>
2016-12-18 15:55:25 -0500

    include vim commentary

--- remote.sh
+++ remote.sh
@@ -16,6 +16,8 @@ git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 
+git clone git clone git://github.com/tpope/vim-commentary.git ~/.vim/bundle/vim-commentary
+
 cp ~/vim-remote/.vimrc ~/.vimrc
 
 rm -rf ~/vim-remote
