8308e0316c6c294a5e0ea3e6666da0c55b31f817
Richard Protasov <richardprotasov@gmail.com>
2016-12-18 16:01:39 -0500

    fix install for vim-commentary

--- remote.sh
+++ remote.sh
@@ -16,7 +16,7 @@ git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 
-git clone git clone git://github.com/tpope/vim-commentary.git ~/.vim/bundle/vim-commentary
+git clone git://github.com/tpope/vim-commentary.git ~/.vim/bundle/vim-commentary
 
 cp ~/vim-remote/.vimrc ~/.vimrc
 
