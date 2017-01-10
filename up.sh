09bb182a744555aeee5aad8365dcb9cee726434f
Richard Protasov <richardprotasov@gmail.com>
2017-01-10 09:58:52 -0500

    Include flag for installing spacevim instead

--- up.sh
+++ up.sh
@@ -3,6 +3,13 @@
 up="$(dirname "$0")"
 mv "$up" ~/
 
+if [ -z "$1" && "$1" = "spacevim" ]
+then
+  curl -sLf https://spacevim.org/install.sh | bash
+  rm -rf ~/up-n-ready-vim
+  exit 0
+fi
+
 rm -rf ~/.vim*
 
 cp ~/up-n-ready-vim/.vimrc ~/.vimrc
